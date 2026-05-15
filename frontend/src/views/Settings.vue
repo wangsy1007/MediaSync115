96238fca5d7f836db60e1aa469db43b37182e12d
wangsy1007 <mrwangshuyin@qq.com>
2026-05-15 14:33:05 +0800

    chore: bump version to 1.1.21

--- frontend/src/views/Settings.vue
+++ frontend/src/views/Settings.vue
@@ -1966,7 +1966,7 @@ const savingUpdateSettings = ref(false)
 const checkingUpdates = ref(false)
 
 const appInfo = ref({
-  currentVersion: '1.1.20',
+  currentVersion: '1.1.21',
   currentImageTag: '',
   currentGitSha: '',
   currentBuildTime: '',
